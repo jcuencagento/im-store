0dc9f7ce0d9f620daecc3d1f729c370d85553690
Javi Cuenca <jcuencagento@gmail.com>
2024-05-19 21:37:42 +0200

    First commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-screen bg-slate-400">
       <body className={inter.className}>{children}</body>
     </html>
   );
